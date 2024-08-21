7c4bf65c2d715a0b2dea2fb2f2b5598901f40eaf
Sanjeev <i.sanjeev.tiwari08@gmail.com>
2024-08-22 01:03:19 +0530

    All files are added

--- src/ex_2108/Lab004.java
+++ src/ex_2108/Lab004.java
@@ -2,7 +2,7 @@ package ex_2108;
 
 public class Lab004 {
     public static void main(String[] Sanjeev) {
-        System.out.println("This (Args)- can be replaced with any thing, it can be in small letter or capital letters");
-        System.out.println("JVM only recognized the ( main ( String[] args) as a main method - Not any other main() function like this");
+        System.out.println("This (Args)- can be replaced with any thing, it can be in small letter or capital letters And this part can be written in both way- (String args[])");
+        System.out.println("JVM only recognized the (public static void main ( String[] args) as a main method - Not any other main() function like this");
     }
 }
