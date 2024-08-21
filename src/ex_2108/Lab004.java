89baa3d782567fe11e86c1ec380b2d5b7be717ca
Sanjeev <i.sanjeev.tiwari08@gmail.com>
2024-08-22 00:56:46 +0530

    All files

---
+++ src/ex_2108/Lab004.java
@@ -0,0 +1,8 @@
+package ex_2108;
+
+public class Lab004 {
+    public static void main(String[] Sanjeev) {
+        System.out.println("This (Args)- can be replaced with any thing, it can be in small letter or capital letters");
+        System.out.println("JVM only recognized the ( main ( String[] args) as a main method - Not any other main() function like this");
+    }
+}
