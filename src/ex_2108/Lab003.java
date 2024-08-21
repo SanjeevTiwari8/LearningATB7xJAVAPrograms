ee26538be513732430cb1de6b56104e3debaafaa
Sanjeev <i.sanjeev.tiwari08@gmail.com>
2024-08-22 00:46:19 +0530

    All files

--- src/ex_2108/Lab003.java
+++ src/ex_2108/Lab003.java
@@ -2,6 +2,14 @@ package ex_2108;
 
 public class Lab003 {
     public static void main(String[] args) {
+        int IntA= 45;
+        int IntB= 75;
+        int ResultAdd= IntA+IntB;
+        int ResultMultiplication= IntA*IntB;
+        int ResultSub= IntA-IntB;
 
+        System.out.println(ResultAdd);
+        System.out.println(ResultSub);
+        System.out.println(ResultMultiplication);
     }
 }
