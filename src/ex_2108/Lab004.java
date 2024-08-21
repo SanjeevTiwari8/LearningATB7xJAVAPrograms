385238d7b7306a6638b1215925b9ff451ce1a4d0
Sanjeev <i.sanjeev.tiwari08@gmail.com>
2024-08-22 01:09:43 +0530

    All files are added

--- src/ex_2108/Lab004.java
+++ src/ex_2108/Lab004.java
@@ -4,5 +4,10 @@ public class Lab004 {
     public static void main(String[] Sanjeev) {
         System.out.println("This (Args)- can be replaced with any thing, it can be in small letter or capital letters And this part can be written in both way- (String args[])");
         System.out.println("JVM only recognized the (public static void main ( String[] args) as a main method - Not any other main() function like this");
+        System.out.println("git commit commands using code");
+        System.out.println("1-step== git add ./");
+        System.out.println("2-step== git commit -m All files are added ");
+        System.out.println("2-step== git push");
+
     }
 }
