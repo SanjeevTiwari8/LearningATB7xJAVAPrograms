816430eb7d049488b79ab25b166dd2574a774a6a
Sanjeev <i.sanjeev.tiwari08@gmail.com>
2024-08-24 23:54:36 +0530

    allfiles

---
+++ src/learning/datatype/Lab008.java
@@ -0,0 +1,20 @@
+package learning.datatype;
+
+public class Lab008 {
+    public static void main(String[] args) {
+        String S1="Sanjeev";
+        int IntA = 10;
+        int IntB = 20;
+        System.out.println(IntA+IntB);
+        System.out.println(IntA+S1);
+        System.out.println(IntB+S1);
+        System.out.println(S1+IntA);
+        System.out.println(S1+IntB);
+        System.out.println(S1+IntB*IntA);
+        System.out.println(IntA*IntB+S1);
+        System.out.println(IntA+IntB+S1);
+        System.out.println(IntA+S1+IntB);
+        System.out.println(S1+IntA+IntB);
+        // concatenation move from left to right, and if first place its get the string, it will assume every thing string now,so in the last print result would be like this ( sanjeev1020, not sanjeev30)
+    }
+}
