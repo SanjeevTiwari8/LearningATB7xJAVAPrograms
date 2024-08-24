a09ec98569e25c95bc42d113d8c28d3b8aef07eb
Sanjeev <i.sanjeev.tiwari08@gmail.com>
2024-08-25 01:06:56 +0530

    all new pro

---
+++ src/learning/datatype/Lab010.java
@@ -0,0 +1,16 @@
+package learning.datatype;
+
+public class Lab010 {
+    public static void main(String[] args) {
+        // Keywords-> Escape characters
+        char c= '\n'; // new line
+        char c1='\t';// new tab (space)
+        char c2='\b'; // backlash delete 1 char
+        char c3='\r'; // backlash delete 1 word.
+        System.out.println("User"+c+"added");
+        System.out.println("User"+c1+"added");
+        System.out.println("User"+c2+"added");
+        System.out.println("User"+c3+"added");
+
+    }
+}
---
+++ src/learning/datatype/Lab011.java
@@ -0,0 +1,18 @@
+package learning.datatype;
+
+public class Lab011 {
+    public static void main(String[] args) {
+        //Operators
+        int A=10;
+        int B=20;
+        // ( = )This is assignment operator.
+        int c= A+B;
+        // here arithmetic operator - ( +, -, /,* )
+        // % - modules
+        System.out.println("Unary operators-(it added on single operand) basically if any int is postive value you don't need to add but if something (-) you have to type before value.");
+        int d= +30;
+        int E= -20;
+        System.out.println(d);
+        System.out.println(E);
+    }
+}
