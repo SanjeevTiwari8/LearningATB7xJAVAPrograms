61ff5e8cc3749b17b0287e7f31073a8b5b8038e8
SanjeevTiwari8 <sanjeev.t.4u@gmail.com>
2024-09-07 01:02:43 +0530

    All files

---
+++ src/important/java/questions/CalculaterNew.java
@@ -0,0 +1,10 @@
+package important.java.questions;
+
+import java.util.Scanner;
+
+public class CalculaterNew {
+    public static void main(String[] args) {
+        Scanner newNumber= new Scanner(System.in);
+
+    }
+}
