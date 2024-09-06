180493ed8c032c90bdcfbff89bf563f74029bcb4
SanjeevTiwari8 <sanjeev.t.4u@gmail.com>
2024-09-07 01:00:18 +0530

    All files

--- src/important/java/questions/CalculatorForOther.java
+++ src/important/java/questions/CalculatorForOther.java
@@ -8,12 +8,12 @@ public class CalculatorForOther {
         Scanner newInput=new Scanner(System.in);        //Other multiplication/divide/ modules
 
         System.out.println("Provide first number");
-        Double firstNumber=newInput.nextDouble();
+        Double firstNumber= newInput.nextDouble();
 
         System.out.println("Provide second number");
-        Double secondNumber = newInput.nextDouble();
+        Double secondNumber= newInput.nextDouble();
 
-        Double multipleResult= firstNumber*secondNumber;
+        Double multipleResult=firstNumber*secondNumber;
         System.out.println("Result of multi:"+ multipleResult);
 
         Double divResult=(firstNumber/secondNumber)*100;
