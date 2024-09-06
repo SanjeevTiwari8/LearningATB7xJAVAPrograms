61dcf06fdb9149dc30507f0e729260da944e072f
Sanjeev <i.sanjeev.tiwari08@gmail.com>
2024-09-06 17:40:47 +0530

    All files

--- src/learning_basic_2024/Lab004GitSteps.java
+++ src/learning_basic_2024/Lab004GitSteps.java
@@ -1,6 +1,6 @@
 package learning_basic_2024;
 
-public class Lab004 {
+public class Lab004GitSteps {
     public static void main(String[] Sanjeev) {
         System.out.println("This (Args)- can be replaced with any thing, it can be in small letter or capital letters And this part can be written in both way- (String args[])");
         System.out.println("JVM only recognized the (public static void main ( String[] args) as a main method - Not any other main() function like this");
