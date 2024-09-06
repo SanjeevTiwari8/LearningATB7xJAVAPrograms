5685ba79fc98d01ee3018eedf154d92c03dbcb0b
SanjeevTiwari8 <sanjeev.t.4u@gmail.com>
2024-09-07 01:05:12 +0530

    All files

--- src/important/java/questions/CalculaterNew.java
+++ src/important/java/questions/CalculaterNew.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class CalculaterNew {
     public static void main(String[] args) {
         Scanner newNumber= new Scanner(System.in);
+        int number= newNumber.nextInt();
+        System.out.println("give some value");
 
     }
 }
