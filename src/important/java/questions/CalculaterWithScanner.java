aad7fa89f77f92b7e8d7106f255c7fe5476759b0
SanjeevTiwari8 <sanjeev.t.4u@gmail.com>
2024-09-07 00:53:16 +0530

    All files

--- src/important/java/questions/CalculaterWithScanner.java
+++ src/important/java/questions/CalculaterWithScanner.java
@@ -1,2 +1,24 @@
-package important.java.questions;public class CalculaterWithScanner {
+package important.java.questions;
+
+import java.util.Scanner;
+
+public class CalculaterWithScanner {
+        public static void main(String[] args) {
+        System.out.println("Add of three numbers");
+        Scanner newinputNumbers= new Scanner(System.in);
+
+        System.out.println("please enter First number");
+        int firstNumber=newinputNumbers.nextInt();
+
+        System.out.println("please enter Second number");
+        int secondNumber=newinputNumbers.nextInt();
+        //User needs to add more data
+
+        System.out.println("please enter Third number");
+        int thirdNumber=newinputNumbers.nextInt();
+
+        int Total= firstNumber+secondNumber+thirdNumber;
+        System.out.println(Total);
+        newinputNumbers.close();
+    }
 }
--- src/important/java/questions/CalculatorForOther.java
+++ src/important/java/questions/CalculatorForOther.java
@@ -1,2 +1,25 @@
-package important.java.questions;public class CalculatorForOther {
+package important.java.questions;
+
+import java.util.Scanner;
+
+public class CalculatorForOther {
+    public static void main(String[] args) {
+
+        Scanner newInput=new Scanner(System.in);        //Other multiplication/divide/ modules
+
+        System.out.println("Provide first number");
+        Double firstNumber=newInput.nextDouble();
+
+        System.out.println("Provide second number");
+        Double secondNumber = newInput.nextDouble();
+
+        Double multipleResult=firstNumber*secondNumber;
+        System.out.println("Result of multi:"+ multipleResult);
+
+        Double divResult=(firstNumber/secondNumber)*100;
+        System.out.println("Result of div:"+ divResult + "%");
+        newInput.close();
+
+
+    }
 }
