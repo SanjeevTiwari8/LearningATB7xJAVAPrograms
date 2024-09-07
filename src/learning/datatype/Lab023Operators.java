12aaae2dca6e92272af511434bd29df0aa0664fc
SanjeevTiwari8 <sanjeev.t.4u@gmail.com>
2024-09-07 11:35:16 +0530

    all

--- src/learning/datatype/Lab023Operators.java
+++ src/learning/datatype/Lab023Operators.java
@@ -8,5 +8,27 @@ public class Lab023Operators {
         boolean d= (a<b);
         System.out.println(c);
         System.out.println(d);
+        // Arithmetic Operators
+        // +,-,%,/,*
+
+        // Compound Assignment Operators
+        // +=, -=, /=, *=
+
+        // Relational Operators
+        // > , < , >= <=, !, != , == (compare two value)
+
+
+        // Logical Operators
+        //  OR or and , || , && , !
+
+        // They will come in the future.  -OOPs concepts
+
+        // new Operator
+        // instanceOf Operator
+
+
+        // Optional to learn - digital Electronics
+        // Bitwise - Not important for Automation
+        // ~ - Bitwise Not, >> - Right shift, << Left Shift, ^ - XOR
     }
 }
